94b8536d11cf919a839669b1c0ceb33ab860ac76
JuanEstuniga <JuanCZuniga@juans-macbook.lan>
2022-04-03 10:56:57 -0400

    fixed localstorage js

--- assets/script/script.js
+++ assets/script/script.js
@@ -22,14 +22,20 @@ $(".time-slot").each(function () {
     }
   });
 
-$(".saveBtn").on("click", function () {
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
     var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id");
+    var time = $(this).parent().attr("id").split("-")[1];
     localStorage.setItem(time, value);
-});
+  });
 
-
-for (var i = 8; i < 20; i++) {
-    $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`));
-}
+$("#hour-09 .time-block").val(localStorage.getItem("09"));
+$("#hour-10 .time-block").val(localStorage.getItem("10"));
+$("#hour-11 .time-block").val(localStorage.getItem("11"));
+$("#hour-12 .time-block").val(localStorage.getItem("12"));
+$("#hour-13 .time-block").val(localStorage.getItem("13"));
+$("#hour-14 .time-block").val(localStorage.getItem("14"));
+$("#hour-15 .time-block").val(localStorage.getItem("15"));
+$("#hour-16 .time-block").val(localStorage.getItem("16"));
+$("#hour-17 .time-block").val(localStorage.getItem("17"));
 
